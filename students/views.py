515232a079691f7eb0f412a399edd95c6940e8a2
Julian Espitaleta <guachamalinga@gmail.com>
2023-03-01 16:24:20 -0500

    Add:Display logic for routes

--- students/views.py
+++ students/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 # Login view
 def login(request):
     return render(request, 'students/login.html')
+
+
+def home(request):
+    return render(request, 'home.html')
--- tests/views.py
+++ tests/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .models import Test
+
 
 # Create your views here.
+
+# View to get all tests
+@login_required
+def get_tests(request):
+    # Get all tests for the logged in user
+    tests = Test.objects.filter(student=request.user.student)
+
+    # Render tests page
+    return render(request, 'tests/MyTests.html', {'tests': tests})
+
+
+# View to get a test
+@login_required
+def get_test(request, test_id):
+
+    # Get test
+    test = Test.objects.get(id=test_id)
+
+    # Check if test is for the logged in user
+    if test.student != request.user.student:
+        # Redirect user to home page
+        return redirect('login')
+
+    # get questions for the test
+    questions = test.question_set.all()
+
+    # Render test page
+    return render(request, 'tests/Test.html', {'test': test, 'questions': questions})
